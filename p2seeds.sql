24eb923180493232d8d0d609328c30eaa83adcd6
Rohit Chhetri <rohit.chhetri@gmail.com>
2019-01-15 20:33:50 -0800

    updating seed information

--- p2seeds.sql
+++ p2seeds.sql
@@ -1,4 +1,12 @@
 --seed 
 USE store;
 INSERT INTO merchant3 (merchant_Id, product_name, department_name, price, stock_quantity,product_desc,photolink,storeid)
-VALUES (1, "Monitor", "Electronics", 149.99, 15,"LG Monitor","https://www.google.com",2)
+VALUES (3, "Monitor", "Electronics", 149.99, 15,"LG Monitor","https://www.google.com",2),
+(3, "Shoes","Shoes", 79, 30,"Nike Shoes","https://www.google.com",2),
+(3, "Computer Table","Furniture", 300,12,"Ikea Table","https://www.ikea.com",2),
+(3, "Keyboard", "Electronics",40, 35,"Samsung Keyboard","https://www.google.com",2),
+(3, "Speaker", "Electronics", 100, 25,"Bose Speaker","https://www.bose.com",2),
+(3, "Titanic DVD", "Movie", 14.99, 11,"Titanic Movie DVD ","https://www.google.com",2),
+(3, "What is Node?", "Book", 30.99, 85,"What is Node Book","https://www.amazon.com",2),
+(3, "Photoshop", "Software", 149.99, 15,"Photoshop","https://www.google.com",2),
+(3, "Guitar", "Musical Instrument", 23.99, 15,"Accostic Guitar","https://www.google.com",2);
