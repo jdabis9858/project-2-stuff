669bf014a616ca4c1ca4c54b842b969fefe8c12e
Rohit Chhetri <rohit.chhetri@gmail.com>
2019-01-15 20:05:26 -0800

    seed structure

--- p2seeds.sql
+++ p2seeds.sql
@@ -1,4 +1,4 @@
 --seed 
 USE store;
-INSERT INTO merchant3 (merchant_Id, product_name, department_name, price, stock_quantity,desc,photolink,storeid)
+INSERT INTO merchant3 (merchant_Id, product_name, department_name, price, stock_quantity,product_desc,photolink,storeid)
 VALUES (1, "Monitor", "Electronics", 149.99, 15,"LG Monitor","https://www.google.com",2)
