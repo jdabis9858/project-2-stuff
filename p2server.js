84f7678713bd084e2a20a8f61007fec10e5dbeb4
jdabis9858 <jdabis4281994@gmail.com>
2019-01-15 20:29:48 -0800

    stuff

--- p2server.js
+++ p2server.js
@@ -25,7 +25,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Root@2019",
+    password: "lambo127",
     database: "store"
 });
 
