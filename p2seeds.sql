40146f4faa04a54d4796d2caf875d06624808f59
Olivia Oxley <ooxley918@gmail.com>
2019-01-15 21:11:58 -0800

    changed values in photo link

--- p2seeds.sql
+++ p2seeds.sql
@@ -1,6 +1,6 @@
 
 
-=======
+
 INSERT INTO merchant3 (merchant_Id, product_name, department_name, price, stock_quantity,product_desc,photolink,storeid)
 
 VALUES (3, "Monitor", "Electronics", 149.99, 15,"LG Monitor","https://www.google.com",2),
@@ -11,10 +11,9 @@ VALUES (3, "Monitor", "Electronics", 149.99, 15,"LG Monitor","https://www.google
 (3, "Titanic DVD", "Movie", 14.99, 11,"Titanic Movie DVD ","https://www.google.com",2),
 (3, "What is Node?", "Book", 30.99, 85,"What is Node Book","https://www.amazon.com",2),
 (3, "Photoshop", "Software", 149.99, 15,"Photoshop","https://www.google.com",2),
-(3, "Guitar", "Musical Instrument", 23.99, 15,"Accostic Guitar","https://www.google.com",2);
+(3, "Guitar", "Musical Instrument", 23.99, 15,"Accostic Guitar","https://www.google.com",2),
 
-VALUES 
-  (2,"halo", "Video Games", 59.99, 150, "Definitive Edition", "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiBrJudqfHfAhVRLX0KHT9RB08QjRx6BAgBEAU&url=https%3A%2F%2Fwww.bestbuy.com%2Fsite%2Ftomb-raider-definitive-edition-playstation-4%2F3089045.p%3FskuId%3D3089045&psig=AOvVaw0bBVZ_fWjvQu-kVtK5pJUS&ust=1547694436779547", 2 ),
+  (2,"halo", "Video Games", 59.99, 150, "Definitive Edition", "", 2 ),
   (2,"Cards Against Humanity", "Board Games", 19.95, 200, "Adult Card Game","  ", 2),
   (2,"Red Dead Redemption II", "Video Games", 59.99, 50, "Western Game", "  ", 2),
   (2,"PS4", "Game Console", 299.95, 75, "Entertainment Console", " ", 2),
@@ -26,5 +25,5 @@ VALUES
   (2,"PS4 Controller", "Accessories", 59.99, 20, "Controller for your PS4 console", " ", 2);
 -- selct all 
 
-  Select * from products; 
+
 
